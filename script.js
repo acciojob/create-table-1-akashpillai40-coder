bd9c96a0132d327f67b5f7935c3db5eaaa92f27e
Akash <akashpillai40@gmail.com>
2026-04-17 11:04:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
+    let table = document.querySelector("table");
+	let button = document.querySelector("input");
+
+
+	let tr = document.createElement("tr");
+		
+	let td1 = document.createElement("td");
+	let td2 = document.createElement("td");
+
+	td1.innerText = "New Cell1";
+	td2.innerText = "New Cell2";
+
+	tr.append(td1, td2);
+
+		table.prepend(tr);
   
   
 }
